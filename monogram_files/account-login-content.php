385dd2dc0b8208269722919d5438f955498ae97e
zawzawzaw <zawzawzaw@gmail.com>
2016-10-28 15:13:54 +0800

    changes

--- monogram_files/account-login-content.php
+++ monogram_files/account-login-content.php
@@ -1,64 +1,69 @@
 <article id="account-login-section-1">
-  <div class="desktop-header-spacer" style="height: 70px;"></div>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-6 col-md-offset-3">
+  <!-- <div class="desktop-header-spacer" style="height: 70px;"></div> -->
+  <div class="container-fluid">
+    <div class="row account-login-content-container">
+      <div class="col-md-6 v-align">
+        <div id="account-login-img-content"></div>
+      </div><!--
+      --><div class="col-md-5 col-md-offset-1 v-align">
         <h1>Member Login</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean iaculis, tellus a efficitur pellente, nibh arcu ultrices mi, ac commodo erat erat sit amet eros. Morbi rutrum arcu.</p>
+
+
+
+      <form action="">
+        <div class="row">
+          <div class="col-md-12">          
+            <div class="inputs">
+              <div class="row">
+                <div class="col-md-2 v-align">
+                  <label for="username" class="for-input">User Name</label>                
+                </div><!--
+                --><div class="col-md-6 v-align">
+                  <input type="text" name="username" class="username-input input-class">
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-2 v-align">
+                  <label for="password" class="for-input">Password</label>                
+                </div><!--
+                --><div class="col-md-7 v-align">
+                  <input type="password" name="password" class="password-input input-class">    
+                  <a href="" class="square-cta">Sign in</a>              
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-3 col-md-offset-2 v-align">
+                  <div class="custom-checkbox-container">
+                    <input type="checkbox" name="remember_me" id="remember_me">
+                    <label for="remember_me" class="custom-checkbox"></label>                  
+                  </div>                
+                  <label for="remember_me" class="for-input remember_me">Remember me</label>
+                </div><!--
+                --><div class="col-md-6 v-align">
+                  <a href="" class="forget-password for-input">Forget password?</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-12">
+            <div class="login-button-container">
+              <a href="" class="square-cta"><i class="fa fa-facebook"></i>Sign in with facebook</a>
+            </div>
+            <span class="login-button-seperator">Or</span>
+            <div class="login-button-container">
+              <a href="" class="square-cta">Create a monogram account</a>
+            </div>
+          </div>
+        </div>
+      </form>
       </div>
     </div>
-
-    <form action="">
-      <div class="row">
-        <div class="col-md-5 col-md-offset-3">          
-          <div class="inputs">
-            <div class="row">
-              <div class="col-md-5 col-md-offset-1 login-label-container v-align">
-                <label for="username" class="for-input">User Name</label>
-              </div><!--
-              --><div class="col-md-6 v-align">
-                <input type="text" name="username" class="input-class">
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-md-5 col-md-offset-1 login-label-container v-align">
-                <label for="password" class="for-input">Password</label>
-              </div><!--
-              --><div class="col-md-6 v-align">
-                <input type="password" name="password" class="input-class">
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-md-6 col-md-offset-6">
-                <div class="custom-checkbox-container">
-                  <input type="checkbox" name="remember_me" id="remember_me">
-                  <label for="remember_me" class="custom-checkbox"></label>                  
-                </div>                
-                <label for="remember_me" class="for-input remember_me">Remember me</label>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col-md-6 col-md-offset-3 c-align">
-          <div class="login-button-container">
-            <a href="" class="square-cta"><i class="fa fa-facebook"></i>Sign in with facebook</a>
-            <a href="" class="square-cta">Sign in with email</a>
-          </div>
-          <span class="login-button-seperator">Or</span>
-          <div class="login-button-container">
-            <a href="" class="square-cta">Create a monogram account</a>
-            <a href="" class="square-cta">Forget your password?</a>
-          </div>
-        </div>
-      </div>
-    </form>
-
 
         
       
   </div>
-  <div class="desktop-header-spacer" style="height: 160px;"></div>
 </article>
