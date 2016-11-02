1c823ee2b005fc36c294cdb17f0aa075984bdd84
zawzawzaw <zawzawzaw@gmail.com>
2016-11-02 16:32:45 +0800

    updated

--- monogram_files/inc/header.php
+++ monogram_files/inc/header.php
@@ -33,9 +33,37 @@
         <div id="desktop-header-cart">
           <ul>
             <li><a href="#" class="text">Currency</a></li>
-            <li><a href="#" class="text">Log in</a></li>
+            <li><a href="#" class="text">Join/Log in</a></li>
             <li><a href="#" class="text">Cart <span>(0)</span></a></li>
             <li><a href="#" class="fa fa-bars"></a></li>
+          </ul>
+        </div>
+
+        <div class="currency-switcher">
+          <div class="currency-form" style="display: none;">
+            <label for="select-currency">Your Currency:</label>
+            <select id="select-currency" name="currency" title="Your Currency" onchange="setLocation(this.value)">
+              <option value="https://www.gryphontea.com/directory/currency/switch/currency/AUD/uenc/aHR0cDovL3d3dy5ncnlwaG9udGVhLmNvbS8,/">AUD</option>
+                <option value="https://www.gryphontea.com/directory/currency/switch/currency/GBP/uenc/aHR0cDovL3d3dy5ncnlwaG9udGVhLmNvbS8,/">GBP</option>
+                <option value="https://www.gryphontea.com/directory/currency/switch/currency/CNY/uenc/aHR0cDovL3d3dy5ncnlwaG9udGVhLmNvbS8,/">CNY</option>
+                <option value="https://www.gryphontea.com/directory/currency/switch/currency/EUR/uenc/aHR0cDovL3d3dy5ncnlwaG9udGVhLmNvbS8,/" selected="selected">EUR</option>
+                <option value="https://www.gryphontea.com/directory/currency/switch/currency/HKD/uenc/aHR0cDovL3d3dy5ncnlwaG9udGVhLmNvbS8,/">HKD</option>
+                <option value="https://www.gryphontea.com/directory/currency/switch/currency/JPY/uenc/aHR0cDovL3d3dy5ncnlwaG9udGVhLmNvbS8,/">JPY</option>
+                <option value="https://www.gryphontea.com/directory/currency/switch/currency/MYR/uenc/aHR0cDovL3d3dy5ncnlwaG9udGVhLmNvbS8,/">MYR</option>
+                <option value="https://www.gryphontea.com/directory/currency/switch/currency/SGD/uenc/aHR0cDovL3d3dy5ncnlwaG9udGVhLmNvbS8,/">SGD</option>
+                <option value="https://www.gryphontea.com/directory/currency/switch/currency/USD/uenc/aHR0cDovL3d3dy5ncnlwaG9udGVhLmNvbS8,/">USD</option>
+            </select>
+          </div>
+          <ul class="currency-select all-dropdown" style="display: block;">
+              <li><a href="#" id="AUD">AUD - Australian Dollar</a></li>
+              <li><a href="#" id="GBP">GBP - British Pound Sterling</a></li>
+              <li><a href="#" id="CNY">CNY - Chinese Yuan</a></li>
+              <li><a href="#" id="EUR">EUR - Euro</a></li>
+              <li><a href="#" id="HKD">HKD - Hong Kong Dollar</a></li>
+              <li><a href="#" id="JPY">JPY - Japanese Yen</a></li>
+              <li><a href="#" id="MYR">MYR - Malaysian Ringgit</a></li>
+              <li><a href="#" id="SGD">SGD - Singapore Dollar</a></li>
+              <li><a href="#" id="USD">USD - US Dollar</a></li>
           </ul>
         </div>
 
