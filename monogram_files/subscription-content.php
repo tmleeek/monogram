07b842a81e49319f34ea4153dd17130405bdaf8a
shocknawe <jairus.aragon@gmail.com>
2016-11-02 15:24:54 +0800

    Subscription Page
    muhahahhaha

---
+++ monogram_files/subscription-content.php
@@ -0,0 +1,173 @@
+<div class="desktop-header-spacer"></div>
+
+<article id="monogram-subscription-title-section">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-2"></div>
+      <div class="col-md-8">
+
+        <div id="monogram-subscription-title">
+          <h1>monogram subscription</h1>
+          <p>A convenient way to save money and time, our subscription service allows you to enjoy a variety of teas at discounted prices, and have them helpfully delivered to your door every month.</p>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</article>
+
+<article id="monogram-subscription-content-section">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-6">
+
+        <!--
+            ___  _
+           / _ \/ |
+          | | | | |
+          | |_| | |
+           \___/|_|
+
+        -->
+
+        <div id="monogram-subscription-content-column-01">
+
+          <div id="monogram-subscription-content-image">
+            <div class="manic-image-container">
+              <img src="" data-image-desktop="assets/images/monogram_subscription/subscription-content.jpg">
+            </div>
+          </div>
+
+        </div> <!-- #monogram-subscription-content-column-01 -->
+
+      </div>
+      <div class="col-md-6">
+
+        <!--
+            ___ ____
+           / _ \___ \
+          | | | |__) |
+          | |_| / __/
+           \___/_____|
+
+        -->
+
+        <div id="monogram-subscription-content-column-02">
+
+          <div id="monogram-subscription-content-copy">
+
+            <div id="monogram-subscription-content-reason-container">
+
+              <div class="row">
+                <div class="col-md-4">
+
+                  <div class="monogram-subscription-content-reason">
+                    <h4>Discover</h4>
+                    <p>A choice of two curated box sets</p>
+                  </div>
+                  
+                </div>
+                <div class="col-md-4">
+
+                  <div class="monogram-subscription-content-reason">
+                    <h4>Pick</h4>
+                    <p>A plan for 1, 3, 6 or 12 months</p>
+                  </div>
+                  
+                </div>
+                <div class="col-md-4">
+
+                  <div class="monogram-subscription-content-reason">
+                    <h4>Receive</h4>
+                    <p>Selection of favourite teas at your door</p>
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
+
+            <div class="table-responsive">
+              <table>
+                <thead>
+                  <tr>
+                    <th>period</th>
+                    <th>price</th>
+                    <th>discount</th>
+                    <th>links</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  <tr>
+                    <td>
+                      <p>1 Month</p>
+                    </td>
+                    <td>
+                      <p>$28.46</p>
+                    </td>
+                    <td>
+                      <p>7%</p>
+                    </td>
+                    <td>
+                      <a href="#" class="arrow-cta">Add to cart</a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <p>3 Months</p>
+                    </td>
+                    <td>
+                      <p>$78.95</p>
+                    </td>
+                    <td>
+                      <p>14%</p>
+                    </td>
+                    <td>
+                      <a href="#" class="arrow-cta">Add to cart</a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <p>6 Months</p>
+                    </td>
+                    <td>
+                      <p>$150.55</p>
+                    </td>
+                    <td>
+                      <p>18%</p>
+                    </td>
+                    <td>
+                      <a href="#" class="arrow-cta">Add to cart</a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <p>12 Months</p>
+                    </td>
+                    <td>
+                      <p>$293.76</p>
+                    </td>
+                    <td>
+                      <p>20%</p>
+                    </td>
+                    <td>
+                      <a href="#" class="arrow-cta">Add to cart</a>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div> <!-- .table-responsive -->
+
+            <div class="cta-container">
+              <a href="#" class="square-cta">layering recommendation</a>
+              <a href="#" class="square-cta">visit tea store</a>
+            </div>
+
+          </div> <!-- #monogram-subscription-content-copy -->
+
+        </div> <!-- #monogram-subscription-content-column-02 -->
+
+      </div>
+    </div>
+  </div>
+</article>
