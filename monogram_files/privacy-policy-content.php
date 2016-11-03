680178dcbc0ec71d1096851038ae68b390f4b590
shocknawe <jairus.aragon@gmail.com>
2016-11-03 22:11:59 +0800

    4 static pages

---
+++ monogram_files/privacy-policy-content.php
@@ -0,0 +1,159 @@
+<!--
+   _____ ___ _____ _     _____
+  |_   _|_ _|_   _| |   | ____|
+    | |  | |  | | | |   |  _|
+    | |  | |  | | | |___| |___
+    |_| |___| |_| |_____|_____|
+
+-->
+
+
+
+<div class="desktop-header-spacer"></div>
+
+<article id="monogram-page-terms-of-use-title-section">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-12">
+        <div id="monogram-page-terms-of-use-title">
+          <h1>monogram privacy policy</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</article>
+
+
+
+
+<!--
+    ____ ___  _   _ _____ _____ _   _ _____
+   / ___/ _ \| \ | |_   _| ____| \ | |_   _|
+  | |  | | | |  \| | | | |  _| |  \| | | |
+  | |__| |_| | |\  | | | | |___| |\  | | |
+   \____\___/|_| \_| |_| |_____|_| \_| |_|
+
+-->
+
+
+
+<article id="monogram-page-terms-of-use-content-section">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-6">
+
+        <!--
+            ____ ___  _    _   _ __  __ _   _    ___  _
+           / ___/ _ \| |  | | | |  \/  | \ | |  / _ \/ |
+          | |  | | | | |  | | | | |\/| |  \| | | | | | |
+          | |__| |_| | |__| |_| | |  | | |\  | | |_| | |
+           \____\___/|_____\___/|_|  |_|_| \_|  \___/|_|
+
+        -->
+
+        <div id="monogram-page-terms-of-use-content-column-01">
+
+          <div class="default-copy">
+            <h4>privacy policy</h4>
+            <p>Your privacy is important to us, which is why over here at Gryphon Tea Company, we are committed to protecting your personal information and we are not in the business of selling it to others. In accordance with Singapore Personal Data Protection Act (“PDPA”), we exercise great care in handling the way personal data is used and shared, making sure to seek your permission before we collect or store any of your data.</p>
+            <p>Coupled with stringent security measures and procedures, rest assured that all personal details submitted to us remain secure. Your visit to our website and submission of any personal details would indicate your acceptance of the terms described in the Privacy Policy. We may however, from time to time, modify these terms. Your continued use of after any modification would therefore, indicate your agreement to the new terms reflected</p>
+
+            
+
+            <h4>personal information</h4>
+            <p>In this Personal Data Protection Policy, “Personal Data” refers to any data, whether true or not, about an individual who can be identified from that data, or from that data and other information to which Gryphon Tea Company has or is likely to have access, including data from our records which may be updated from time to time.</p>
+            <p>Such personal information may include your name, telephone number, mailing address, email address, network data and any other information relating to any individuals you may have provided in any forms, or through any forms of interaction with Gryphon Tea Company.</p>
+
+            <h4>collection and use of personal data</h4>
+            <p>We collect and use the personal data you enter on the website or disclose to Gryphon Tea Company in various ways.</p>
+            <p>When navigating our website, you may sometimes be asked to provide personal information from which you can be identified. These data will only be obtained with your consent. An individual’s personal information may include, but not limited to, his/her name, contact details (email address, mailing address and billing address) and other personal information we collect in the course of a transaction or that you provide when you contact Gryphon Tea Company.</p>
+            <p>Such personal data may be obtained through www.gryphontea.com account registration process, or when updating account details. Personal data may also be collected when you request or enquire a product or service, provide a product or service, complete a survey or questionnaire, enter a competition, participate in forums or when you communicate with Gryphon Tea Company in person or writing.</p>
+
+
+            <h4>your personal data maybe collected when:</h4>
+
+            <h4 class="sub-version">UPDATING PERSONAL INFORMATION</h4>
+            <p>Gryphon Tea Company endeavours to ensure that the personal information we hold is accurate, complete and up-to-date. You can update your www.gryphontea.com account information via the “Members Log In” section on our website or by writing in to us.</p>
+            <h4 class="sub-version">ACCOUNT REGISTRATION</h4>
+            <p>You become a member of www.gryphontea.com once you sign up for an account with us. Gryphon Tea Company has a registration process through which you voluntarily sign up to access the members-only features of our website. Your name, email address, telephone number and mailing address will have to be provided during the registration process. To ensure maximum security, we strongly suggest that you create a unique username with a password.</p>
+            <h4 class="sub-version">NEWSLETTER SUBSCRIPTION</h4>
+            <p>In order to receive promotional or informational e-newsletters from time to time, your email address will be requested upon whenever you subscribe to our free e-newsletters. This will allow us to e-mail the copy over to you. You may opt out of receiving future mailings or cancel your subscription(s) at any time.</p>
+            <h4 class="sub-version">CONTESTS AND PROMOTIONS</h4>
+            <p>When you enter a contest or take part in a promotion, we may ask for your name, mailing address and email address. Should a third party sponsor the contest or promotion, that party will have access to this information. This information is required in order to notify our winners and, when applicable, ship the winners' prizes.</p>
+            
+
+
+
+          </div>
+
+        </div> <!-- #monogram-page-terms-of-use-content-column-01 -->
+      </div> <!-- col-md-6 -->
+      <div class="col-md-6">
+
+        <!--
+            ____ ___  _    _   _ __  __ _   _    ___ ____
+           / ___/ _ \| |  | | | |  \/  | \ | |  / _ \___ \
+          | |  | | | | |  | | | | |\/| |  \| | | | | |__) |
+          | |__| |_| | |__| |_| | |  | | |\  | | |_| / __/
+           \____\___/|_____\___/|_|  |_|_| \_|  \___/_____|
+
+        -->
+
+        <div id="monogram-page-terms-of-use-content-column-02">
+
+          <div class="default-copy">
+
+            <h4 class="sub-version">PUBLIC FORUMS</h4>
+            <p>Please note that any information disclosed in public forums becomes public information. Exercise caution when deciding to disclose your personal information. As any public forums are open for users to interact with one another, Gryphon Tea Company expects all its users to respect the rights of others.</p>
+            <p>Note: Gryphon Tea Company uses PayPal as a payment gateway, therefore we do not keep payment information such as Credit Card number in our database.</p>
+            
+            <h4>purpose of collecting your personal data</h4>
+            <p>We collect personal data in order to provide our products and extend our services to you. Your personal data allows for us to improve our products and services that will help to serve you better. Thus, the purpose of the collection of personal data may include but not limited to:</p>
+
+            <ol>
+              <li>Provide you with products or services you have requested</li>
+              <li>Personalise and customise your experiences with us such as the saving of shopping cart information</li>
+              <li>Help manage and enhance our services</li>
+              <li>Communicate with you</li>
+              <li>Provide you with ongoing information about the opportunities at Gryphon Tea Company that we believe may be of interest to you</li>
+              <li>Give you the opportunity from time to time to receive emails and e-newsletters from Gryphon Tea Company</li>
+            </ol>
+
+            <h4>use of cookies and ip address</h4>
+            <p>Gryphon Tea Company also collects statistical information of a non-personal nature. Such data is collected automatically through a cookie located in your browser, which allows us to measure the number of site visits, average time spent, page views and other useful statistical information relating to the access of www.gryphontea.com. This method of collection is performed by most webpages and will not allow you to be personally identified, as it contains only statistical information.</p>
+            <p>Gryphon Tea Company may use the data in cookies to develop focused advertising targeted at a fixed group of consumers. In some cases where a webpage contains more than one cookie, the data gathered from these cookies would then be used to build statistical and demographical information. Such data are incapable of revealing your personal details to advertisers and any other third parties for any marketing or promotional purposes. You may however, configure your browser so that you are notified before a cookie is downloaded, or so that your browser does not accept cookies.</p>
+            <p>Cookies allow you to take advantage of some of Gryphon Tea Company’s coolest services, and we recommend that you leave them turned on. If you intend to disable your cookies, simply refer to the “Help” menu within your Internet browser. Note that by choosing to disable the cookie function on your browser, some features on the website may not function, and your options may be limited.</p>
+            <p>Gryphon Tea Company also collects Internet Protocol (IP) addresses to help identify problems with our server and for the purposes of system administration. This unique identifier allows us to identify repeated site visitors, help keep track of users’ shopping cart contents and to gather demographic information to better understand the types of consumers that are interested in our site.</p>
+
+
+
+            <h4>disclosure of personal data</h4>
+            <p>Gryphon Tea Company will exercise caution and take reasonable effort to protect your personal data against unauthorised disclosure. Subject to the provisions of any applicable law, your personal data may be disclosed in the following situations but not limited to:</p>
+            <ol>
+              <li>Processing, delivering, billing for, and collecting payment for purchased products (this excludes credit card information as Gryphon Tea Company uses a third party service provider as www.gryphontea.com’s payment gateway)</li>
+              <li>Facilitating your purchase with our affiliated service providers, third parties engaged to perform functions on its behalf. This can include payment processing, order fulfilment and shipment</li>
+              <li>Collecting feedback</li>
+              <li>Updating you on changes on our services or products, including new promotions</li>
+            </ol>
+
+
+            
+
+
+
+            
+          </div>
+
+        </div> <!-- #monogram-page-terms-of-use-content-column-02 -->
+      </div> <!-- col-md-6 -->
+    </div> <!-- row -->
+  </div> <!-- container-fluid -->
+</article>
+
+
+
+<script type="text/javascript">
+  jQuery(document).ready(function($) {
+    jQuery('body, html').addClass('monogram-page-privacy-policy');
+  });
+</script>
